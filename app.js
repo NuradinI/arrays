027a0002db1f074c252e351c96eda5e5eaca450a
Nuradin Ibrahim <nuradinibrahim@Nuradins-MacBook-Air.local>
2020-08-21 15:52:08 -0600

    added loop with reapeating numbers 1-10

---
+++ app.js
@@ -0,0 +1,4 @@
+var users = ("hello there", "twitter is awesome", "im great", "i have hair", "i have a keyboard", "i cut my finger", "instagram is great", "black holes exist", "i have two monitors", "rest in peace kobe", "i have a macbook");
+for ( users = 1; users <=10; users++) {
+    console.log(users)
+}
