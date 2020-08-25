7cba85e319db4f39a63736810e186f003c04b816
Nuradin Ibrahim <nuradinibrahim@Nuradins-MacBook-Air.local>
2020-08-24 18:35:53 -0600

    Added in new tweets with usernames, age, tweet and date

--- app.js
+++ app.js
@@ -1,4 +1,68 @@
-var users = ("hello there", "twitter is awesome", "im great", "i have hair", "i have a keyboard", "i cut my finger", "instagram is great", "black holes exist", "i have two monitors", "rest in peace kobe", "i have a macbook");
-for ( users = 1; users <=10; users++) {
-    console.log(users)
+
+const myLocks = [
+    {
+        Tweet : 'Nba2k21 demo is terrible',
+        username    : 'kobefan24',
+        created_at  : '02/23/20',
+        age : 18
+    },
+    {
+        Tweet       : 'Donald Trump!',
+        username    : 'mrrichards',
+        created_at  : '08/21/15',
+        age         : 63
+    },
+    {
+        Tweet : 'Wallmart has amazing clothes',
+        username    : 'karen210',
+        created_at  : '12/13/10',
+        age : 23
+    },
+    {
+        Tweet : 'the new iphone looks awesome',
+        username    : 'trulycomp',
+        created_at  : '22/22/12',
+        age : 16
+    },
+    {
+        Tweet : 'how do I fix my keyboard',
+        username    : 'loadingintolife',
+        created_at  : '04/11/20',
+        age : 56
+    },
+    {
+        Tweet : 'lol look at all the 19s',
+        username    : 'kelly19',
+        created_at  : '19/19/19',
+        age : 22 
+    },
+    {
+        Tweet : 'candy crush is so fun',
+        username    : 'candycrusher',
+        created_at  : '28/12/16',
+        age : 49
+    },
+    {
+        Tweet : 'xbox is better than ps4',
+        username    : 'xboxplayer',
+        created_at  : '01/17/12',
+        age : 32
+    },
+    {
+        Tweet : 'its 11:11 make a wish',
+        username    : 'xx_cassandra_xx',
+        created_at  : '11/11/11',
+        age : 17
+    },
+    {
+        Tweet : 'Brawlhalla is a good game',
+        username    : 'mark_12',
+        created_at  : '23/19/10',
+        age : 27
+    }
+]
+
+for (let i=0; i <myLocks.length; i++) {
+var locks = myLocks[i]
+console.log(locks)
 }
