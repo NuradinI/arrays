8a59330f3badfed9f4848d627d7031b5cb7e3829
Nuradin Ibrahim <nuradinibrahim@Nuradins-MacBook-Air.local>
2020-09-03 10:41:14 -0600

    Added events

--- app.js
+++ app.js
@@ -1,84 +1,18 @@
+const button = document.querySelector('button');
 
-var myLocks = checkAge[
-    {
-        Tweet : 'Nba2k21 demo is terrible',
-        username    : 'kobefan24',
-        created_at  : '02/23/20',
-        age : 18
-    },
-    {
-        Tweet       : 'Donald Trump!',
-        username    : 'mrrichards',
-        created_at  : '08/21/15',
-        age         : 63
-    },
-    {
-        Tweet : 'Wallmart has amazing clothes',
-        username    : 'karen210',
-        created_at  : '12/13/10',
-        age : 23
-    },
-    {
-        Tweet : 'the new iphone looks awesome',
-        username    : 'trulycomp',
-        created_at  : '22/22/12',
-        age : 16
-    },
-    {
-        Tweet : 'how do I fix my keyboard',
-        username    : 'loadingintolife',
-        created_at  : '04/11/20',
-        age : 56
-    },
-    {
-        Tweet : 'lol look at all the 19s',
-        username    : 'kelly19',
-        created_at  : '19/19/19',
-        age : 22 
-    },
-    {
-        Tweet : 'candy crush is so fun',
-        username    : 'candycrusher',
-        created_at  : '28/12/16',
-        age : 49
-    },
-    {
-        Tweet : 'xbox is better than ps4',
-        username    : 'xboxplayer',
-        created_at  : '01/17/12',
-        age : 32
-    },
-    {
-        Tweet : 'its 11:11 make a wish',
-        username    : 'xx_cassandra_xx',
-        created_at  : '11/11/11',
-        age : 17
-    },
-    {
-        Tweet : 'Brawlhalla is a good game',
-        username    : 'mark_12',
-        created_at  : '23/19/10',
-        age : 27
+button.addEventListener('click', function(){
+    document.querySelector('button').style.backgroundColor = '#dd2c00'
+})
+
+document.getElementById('naruto').addEventListener('mouseenter', mouseEnter)
+function mouseEnter() {
+    document.getElementById('naruto').innerHTML = '<img src="https://artfiles.alphacoders.com/106/106054.jpg">'
+}
+
+const bodyElement = document.getElementById('site')
+
+bodyElement.addEventListener('keypress', function(eventDetails){
+    if(eventDetails.key == 'h'){
+        bodyElement.style.backgroundColor = '#e0ece4';
     }
-]
-
-for (let i=0; i <myLocks.length; i++) {
-var locks = myLocks[i]
-console.log(locks)
-}
-
-
-function checkAge(age){
-   checkAge; return age>=18;
-
-}if(true){
-   checkAge; return age >18,
-   myLocks.filter(tweet >18)
-}
-
-
-
-
-    
-
-    
+})
